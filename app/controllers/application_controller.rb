b81a6c65dca20d6af058e45ed5afce63330972f7
Neylon Santos <neylonssantos@gmail.com>
2021-06-08 00:11:08 -0300

    config endpoint to change password

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::API
 
   protected
 
-  de configure_permitted_parameters
+  def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
   end
 end
