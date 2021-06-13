141f35656700c8e193ba2bbf00e50755171ddd7b
Neylon Santos <neylonssantos@gmail.com>
2021-06-13 20:44:29 -0300

    Create game model

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,7 +2,10 @@ FactoryBot.define do
   factory :product do
     sequence(:name) { |n| "Product #{n}" }
     description { Faker::Lorem.paragraph }
-    price { "9.99" }
     price { Faker::Commerce.price(range: 100.0..400.0) }
+
+    after :build do |product|
+      product.productable = create(:game)
+    end
   end
 end
